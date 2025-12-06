f47c104964a2ac8110786fee32d570c929e7a721
AoEiuV020 <aoeiuv020@gmail.com>
2025-12-06 18:52:46 +0800

    添加空dart包
    准备封装http代理，

---
+++ packages/bilibili_live_api_server/example/bilibili_live_api_server_example.dart
@@ -0,0 +1,6 @@
+import 'package:bilibili_live_api_server/bilibili_live_api_server.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/bilibili_live_api_server/test/bilibili_live_api_server_test.dart
@@ -0,0 +1,16 @@
+import 'package:bilibili_live_api_server/bilibili_live_api_server.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
