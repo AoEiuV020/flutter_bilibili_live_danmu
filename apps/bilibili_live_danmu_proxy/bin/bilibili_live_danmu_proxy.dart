56ed3a111a4e5bba4934d3a0af838263ac48c835
AoEiuV020 <aoeiuv020@gmail.com>
2025-12-06 18:47:07 +0800

    添加空dart应用
    准备做接口代理，

---
+++ apps/bilibili_live_danmu_proxy/bin/bilibili_live_danmu_proxy.dart
@@ -0,0 +1,5 @@
+import 'package:bilibili_live_danmu_proxy/bilibili_live_danmu_proxy.dart' as bilibili_live_danmu_proxy;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bilibili_live_danmu_proxy.calculate()}!');
+}
---
+++ apps/bilibili_live_danmu_proxy/test/bilibili_live_danmu_proxy_test.dart
@@ -0,0 +1,8 @@
+import 'package:bilibili_live_danmu_proxy/bilibili_live_danmu_proxy.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
